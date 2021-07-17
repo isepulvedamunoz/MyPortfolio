edd8262006b62f9d162c51bee67fa080e964d774
Ignacio Sepulveda <isepulvedamunoz@gmail.com>
2021-07-17 11:34:28 -0700

    configurado archivo url

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 from main_page import views
 
 urlpatterns = [
-    path('', views.index),
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("admin/", admin.site.urls),
     
 ] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
