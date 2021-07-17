94bf4aea219d490693ee1bfbc23186c2da3c864a
Ignacio Sepulveda <isepulvedamunoz@gmail.com>
2021-07-16 22:29:40 -0700

    nuevo cambio

--- myportfolio/myportfolio/urls.py
+++ myportfolio/myportfolio/urls.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 from main_page import views
 
